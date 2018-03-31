b07acfee9eaad721f6bc3cdaa02a1676220fa540
Marat Valiev <mz.valiev@gmail.com>
2018-03-30 21:37:42 -0400

    Refactored js file

--- js/index.js
+++ js/index.js
@@ -1,19 +1,21 @@
 //Random quotes
-$(document).ready(function() {
-  $("#quote-button").on("click", function() {
-    $.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=30&callback=", function(a) {
-      let content = "",
-        author = "",
-        num = Math.floor(Math.random() * 30);
-      author = a[num]["title"];
-      content = a[num]["content"];
-      $("#quote").html(content);
-      $("#author").html("- " + author);
+const api = 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=30&callback=';
+
+$(function() {
+  $('#quote-button').on('click', function() {
+    $.getJSON(api, function(a) {
+      let content = '';
+      let author = '';
+      const num = Math.floor(Math.random() * 30);
+      author = a[num].title;
+      content = a[num].content;
+      $('#quote').html(content);
+      $('#author').html('- ' + author);
     });
   });
 });
 //Share quote on Twitter
-$("#twitter-button").on("click", function() {
+$('#twitter-button').on('click', function() {
   const text = `${$('#quote').text()} ${$('#author').html()}`;
   const link = `https://twitter.com/intent/tweet?text=${text}&hashtags=freeCodeCamp`;
   window.open(link, '_blank');
