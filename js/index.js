7579152d25edb59d2296543fff8920ff8376dee6
Marat Valiev <mz.valiev@gmail.com>
2018-04-22 21:34:44 -0400

    Implement async/await

--- js/index.js
+++ js/index.js
@@ -1,22 +1,29 @@
 //Random quotes
 const api = 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=30&callback=';
 
-$(function() {
-  $('#quote-button').on('click', function() {
-    $.getJSON(api, function(a) {
-      let content = '';
-      let author = '';
-      const num = Math.floor(Math.random() * 30);
-      author = a[num].title;
-      content = a[num].content;
-      $('#quote').html(content);
-      $('#author').html('- ' + author);
+async function getQuote() {
+  try {
+    await axios.get(api)
+      .then((getData) => {
+        $('#quote-button').on('click', function () {
+          let content = '';
+          let author = '';
+          const num = Math.floor(Math.random() * 30);
+          author = getData.data[num].title;
+          content = getData.data[num].content;
+          $('#quote').html(content);
+          $('#author').html(`- ${author}`);
+        })
+      });
+    //Share quote on Twitter
+    $('#twitter-button').on('click', function () {
+      const text = `${$('#quote').text()} ${$('#author').html()}`;
+      const link = `https://twitter.com/intent/tweet?text=${text}&hashtags=freeCodeCamp`;
+      window.open(link, '_blank');
     });
-  });
-});
-//Share quote on Twitter
-$('#twitter-button').on('click', function() {
-  const text = `${$('#quote').text()} ${$('#author').html()}`;
-  const link = `https://twitter.com/intent/tweet?text=${text}&hashtags=freeCodeCamp`;
-  window.open(link, '_blank');
-});
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getQuote();
